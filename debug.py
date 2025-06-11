8fbbad3278ccabf0a9980a0b5b1c0a84ac51dcc5
NirshalChandraSekar <nirshalniru@gmail.com>
2025-06-11 17:20:58 -0500

    script to view combined point cloud

--- debug.py
+++ debug.py
@@ -4,8 +4,28 @@ import numpy as np
 # import mediapipe as mp
 import open3d as o3d
 import pyrealsense2 as rs
+import os
 
 dataset_path = 'dataset/trial_dataset/check_video_data.h5'  # <- change this to your actual path if needed
+serial_numbers = ['213522250729', '213622251272']  # <- update these to your recorded camera serial numbers
+
+'''
+Camera 213622251272 Intrinsics:
+  Width: 1280
+  Height: 800
+  Focal Length (fx, fy): (645.9054565429688, 645.9054565429688)
+  Principal Point (ppx, ppy): (646.1998901367188, 403.8904113769531)
+  Distortion Model: distortion.brown_conrady
+  Distortion Coefficients: [0.0, 0.0, 0.0, 0.0, 0.0]
+Camera 213522250729 Intrinsics:
+  Width: 1280
+  Height: 720
+  Focal Length (fx, fy): (649.6065063476562, 649.6065063476562)
+  Principal Point (ppx, ppy): (650.9663696289062, 364.2262878417969)
+  Distortion Model: distortion.brown_conrady
+  Distortion Coefficients: [0.0, 0.0, 0.0, 0.0, 0.0]
+'''
+
 serial_numbers = ['213622251272', '213522250729']  # <- update these to your recorded camera serial numbers
 
 def save_video_from_dataset(dataset_path, output_video_path, serial_numbers):
@@ -120,22 +140,6 @@ def view_recorded_stream(dataset_path, serial_numbers):
     cv2.destroyAllWindows()
 
 
-def view_point_cloud(dataset_path, serial_number):
-    with h5py.File(dataset_path, 'r') as dataset:
-        color_image = dataset[serial_number]['color'][str(0)][()]
-        depth_image = dataset[serial_number]['depth'][str(0)][()]
-
-        # Create point cloud from depth image
-        intrinsics = o3d.camera.PinholeCameraIntrinsic(640, 480, 525.0, 525.0, 320.0, 240.0)
-        pcd = o3d.geometry.PointCloud.create_from_depth_image(
-            o3d.geometry.Image(depth_image), intrinsics)
-
-        # Color the point cloud
-        pcd.colors = o3d.utility.Vector3dVector(color_image.reshape(-1, 3) / 255.0)
-
-        o3d.visualization.draw_geometries([pcd])
-
-
 def get_camera_intrinsics(serial_numbers=None):
     # Create a context object
     ctx = rs.context()
@@ -164,11 +168,105 @@ def get_camera_intrinsics(serial_numbers=None):
         except StopIteration:
             print(f"Camera with serial {serial} not found.")
 
-# save_video_from_dataset(dataset_path, combined_video.MP4, serial_numbers)
-# run_mediapipe_on_videos(dataset_path, 'mediapipe_combined_video.MP4', serial_numbers)
-# view_point_cloud(dataset_path, serial_numbers[0])
 
-get_camera_intrinsics(serial_numbers)
+def view_point_cloud_single_camera(dataset_path = 'dataset/trial_dataset/check_video_data.h5', serial_number = serial_numbers[0]):
+    with h5py.File(dataset_path, 'r') as dataset:
+        color_image = dataset[serial_number]['color'][str(20)][()]
+        cv2.imshow('Color Image', color_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)  # Convert to RGB for Open3D
+        depth_image = (dataset[serial_number]['depth'][str(20)][()] * 0.001).astype(np.float32)  # Convert depth to meters
+        intrinsics_ = np.load('data/213622251272_intrinsics.npy', allow_pickle=True).item()
+        intrinsic = o3d.camera.PinholeCameraIntrinsic(
+            width=intrinsics_['width'],
+            height=intrinsics_['height'],
+            fx=intrinsics_['fx'],
+            fy=intrinsics_['fy'],
+            cx=intrinsics_['ppx'],
+            cy=intrinsics_['ppy'])
+
+        
+        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            o3d.geometry.Image(color_image),
+            o3d.geometry.Image(depth_image),
+            depth_scale=1,  # Adjust based on your depth image scale
+            depth_trunc=3.0,  # Truncate depth values beyond this distance
+            convert_rgb_to_intensity=False)
+        
+        pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, intrinsic)
+
+        o3d.visualization.draw_geometries([pcd])
+
+
+'''
+Transformation matrix:
+
+[ -0.986436, -0.1571164, -0.04752302, -0.04698686; 0.1640189, -0.9548615, -0.2476637, -0.07646833; -0.006465859, -0.2520991, 0.9676798, -0.07681673; 0, 0, 0, 1 ]
+'''
+
+def view_combined_point_cloud(dataset_path, serial_numbers = serial_numbers):
+    with h5py.File(dataset_path, 'r') as dataset:
+        
+        color_image_master = cv2.imread(os.path.join('data', serial_numbers[0] + '_color.png'))
+        color_image_master = cv2.cvtColor(color_image_master, cv2.COLOR_BGR2RGB)
+        depth_image_master = (np.load(os.path.join('data', serial_numbers[0] + '_depth.npy'), allow_pickle=True) * 0.001).astype(np.float32)  # Convert depth to meters
+        intrinsic_master = np.load(os.path.join('data', serial_numbers[0] + '_intrinsics.npy'), allow_pickle=True).item()
+        intrinsic_master = o3d.camera.PinholeCameraIntrinsic(
+            width=intrinsic_master['width'],
+            height=intrinsic_master['height'],
+            fx=intrinsic_master['fx'],
+            fy=intrinsic_master['fy'],
+            cx=intrinsic_master['ppx'],
+            cy=intrinsic_master['ppy'])
+        rgbd_image_master = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            o3d.geometry.Image(color_image_master),
+            o3d.geometry.Image(depth_image_master),
+            depth_scale=1,  # Adjust based on your depth image scale
+            depth_trunc=3.0,  # Truncate depth values beyond this distance
+            convert_rgb_to_intensity=False)
+        pcd_master = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image_master, 
+                                                                    intrinsic_master,
+                                                                    extrinsic=np.eye(4))
+        
+
+        color_image_slave = cv2.imread(os.path.join('data', serial_numbers[1] + '_color.png'))
+        color_image_slave = cv2.cvtColor(color_image_slave, cv2.COLOR_BGR2RGB)
+        depth_image_slave = (np.load(os.path.join('data', serial_numbers[1] + '_depth.npy'), allow_pickle=True) * 0.001).astype(np.float32)  # Convert depth to meters
+        intrinsic_slave = np.load(os.path.join('data', serial_numbers[1] + '_intrinsics.npy'), allow_pickle=True).item()
+        intrinsic_slave = o3d.camera.PinholeCameraIntrinsic(
+            width=intrinsic_slave['width'],
+            height=intrinsic_slave['height'],
+            fx=intrinsic_slave['fx'],
+            fy=intrinsic_slave['fy'],
+            cx=intrinsic_slave['ppx'],
+            cy=intrinsic_slave['ppy'])
+        rgbd_image_slave = o3d.geometry.RGBDImage.create_from_color_and_depth(
+            o3d.geometry.Image(color_image_slave),
+            o3d.geometry.Image(depth_image_slave),
+            depth_scale=1,  # Adjust based on your depth image scale
+            depth_trunc=3.0,  # Truncate depth values beyond this distance
+            convert_rgb_to_intensity=False)
+        transform_matrix = np.array([
+            [-0.986436, -0.1571164, -0.04752302, -0.04698686],
+            [0.1640189, -0.9548615, -0.2476637, -0.07646833],
+            [-0.006465859, -0.2520991, 0.9676798, -0.07681673],
+            [0, 0, 0, 1]
+        ], dtype=np.float32)
+        
+        # transform_matrix = np.linalg.inv(transform_matrix)  # Invert the transformation matrix for the slave camera
+
+        pcd_slave = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image_slave, 
+                                                                    intrinsic_slave,
+                                                                    extrinsic=transform_matrix)
+        
+        o3d.visualization.draw_geometries([pcd_master, pcd_slave])   
+
+
+
+# view_point_cloud_single_camera(dataset_path)
+view_combined_point_cloud(dataset_path)
+
 
 
 
